654aafb9315eee7eb8080091ba3b94242f49db22
{nazar} <{nazar.baranec@mail.ru}>
2023-11-04 14:44:33 +0300

    updated the code for task number 7

--- src/main/java/ru/Baranets/homeworks7.java
+++ src/main/java/ru/Baranets/homeworks7.java
@@ -5,74 +5,46 @@ import java.util.Arrays;
 
 public class homeworks7 {
 
-
     public static void main(String[] args) {
-        sumOfPositiveElements(new int[][]{{1, 5, -10, 0}, {1, 2, 0, -8}});
+        sumOfPositiveElements(new int[][]{{1, 5,}, {1, 1, 1, 1}});
         printSquare(5);
         diagonalSquare(new int[5][5]);
         findMax(new int[][]{{1, 2, 3, 4, 5, 6, 7}, {1, 2, 6, 7, 8, 9, 1}});
         sumSecondString(new int[][]{{1, 2, 3}, {5, 5, 5}});
-
-
     }
 
     public static void sumOfPositiveElements(int[][] array) {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] > 0) {
-                    sum += array[i][j];
-                }
-
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > 0) sum += array[i][j];
             }
-
         }
-        System.out.println(sum);
-
-
+        return;
     }
 
     public static void printSquare(int size) {
-
-
-        char[][] array = new char[size][size];
-
-        for (int x = 0; x < array.length; x++) {
-            for (int y = 0; y < array.length; y++) {
-                array[x][y] = '*';
-                System.out.print(array[x][y] + "  ");
-
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                System.out.print(" * ");
             }
             System.out.println();
-
         }
         System.out.println();
-
-
     }
 
     public static void diagonalSquare(int[][] array) {
-        for (int x = 0; x < array.length; x++) {
-            for (int y = 0; y < array.length; y++) {
-                array[x][y] = 1;
-                if (y == x) {
-                    array[x][y] = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = 1;
+                if (j == i) {
+                    array[i][j] = 0;
                 }
-                if (y + x == array.length - 1) {
-                    array[x][y] = 0;
-                }
-
-                System.out.print(array[x][y] + "  ");
-
+                System.out.print(array[i][j] + "  ");
             }
             System.out.println();
-
-
         }
-
-
     }
-
 
     public static void findMax(int[][] array) {
         int maxValue = array[0][0];
@@ -84,25 +56,19 @@ public class homeworks7 {
             }
         }
         System.out.println(maxValue);
-
     }
 
-    public static void sumSecondString(int[][] array) {
+    public static int sumSecondString(int[][] array) {
         if (array.length >= 2) {
             int str2 = 0;
             for (int i = 0; i < array[1].length; i++) {
                 str2 += array[1][i];
             }
-            System.out.println(str2);
-
-
+            return str2;
         } else {
-            System.out.println(-1);
+            return -1;
         }
-
-
     }
-
 }
 
 
